a6d6c6311707cb3aaae1e43e7c1bea89c9c5c6be
Saqlain Nawaz <saqlain@ultranetgh.com>
2026-07-13 16:02:32 +0500

    Initial Commit

---
+++ UltraSIP/UltraSIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltraSIPApp.swift
+//  UltraSIP
+//
+//  Created by Developer on 13/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct UltraSIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
